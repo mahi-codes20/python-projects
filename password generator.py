4833097aef8542f97fbfab917c00ad5d53a321a0
mahi-codes20 <mahikachhawa20@gmail.com>
2026-07-21 22:13:14 +0530

    Added password_generator.py

---
+++ password generator.py
@@ -0,0 +1,13 @@
+#project 4
+#password generator
+#created by mahi
+letters = [ "a" , "b" ,"c" , "d" , "e" ,"f" , "g" ,"h" , "i" , "j", "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s " , "t" , "u" , "v" , "w" , "x" , "y" , "z" ]
+lenght = int(input("enter password lenght:"))
+password = " "
+if password == "abcdefgh":
+	print ("yess correct")
+else:
+	print ("absonutely wrong")
+	
+	
+
